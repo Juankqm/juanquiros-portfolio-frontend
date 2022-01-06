065fb0bd28d4885e96e12e2daae76c05e2bd8468
Juan Quirós <Juan.kqm96@gmail.com>
2022-01-05 22:17:19 -0600

    Profile component
    Full profile component with its styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
 import './App.css';
+import Home from './Components/Home/Home'
 
 function App() {
   return (
     <div className="App">
-    
+      <Home />
     </div>
   );
 }
 
 export default App;
+
+
+// https://www.skillshare.com/classes/The-Ultimate-React-Js-Responsive-Portfolio-Website/1392304277?teacherRef=621413323&via=teacher-referral&utm_campaign=teacher-referral&utm_source=ShortUrl&utm_medium=teacher-referral
+// https://www.youtube.com/watch?v=ilw_g_rxtVc&t=1354s
