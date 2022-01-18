3bc9103d74b0e8801cca773db7a36f57ccb53bc7
Juan Quirós <Juan.kqm96@gmail.com>
2022-01-17 21:17:52 -0600

    Create About Me Component
    About Me component finished as well as its style for desktop and responsive design.

--- src/utilities/commonUtils.js
+++ src/utilities/commonUtils.js
@@ -1,10 +1,15 @@
 import Home from '../Components/Home/Home'
+import AboutMe from '../Components/AboutMe/AboutMe'
 //import Resume from '../Components/Resume/Resume'
 
 export const TOTAL_SCREENS =  [
     {
         screen_name: "Home",
         component: Home
+    },
+    {
+        screen_name: "AboutMe",
+        component: AboutMe
     }
 ]
 
